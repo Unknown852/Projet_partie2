890d2109fb4fba5e39c49c274b4cf529fadc41d9
Unknown852 <unknown852@gmail.com>
2017-09-28 20:34:53 +0200

    Changement PIN Blink + réduction délai de clignotement

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,13 +1,13 @@
-int pinBlink = 13;
+int pinBlink = 7;
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pin 7 as an output.
   pinMode(pinBlink, OUTPUT);
 }
 
 void loop() {
   digitalWrite(pinBlink, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(pinBlink, LOW);
-  delay(1000);
+  delay(500);
 }
